d08d59a8fa07bd17ea4dd14680a0a437de027282
Corne <corne.ackerman1104@gmail.com>
2024-04-29 09:23:55 +0200

    creating joke.js and main.js

---
+++ .github/actions/joke-action/joke.js
@@ -0,0 +1,18 @@
+const request = requier("request-promise");
+
+const options = {
+    method: "GET",
+    uri: "https://icanhazdadjoke.com/",
+    headers: {
+        Accept: "application/json",
+        "User-Agent": "Writing javascript action github skulls course.",
+    },
+    json: true
+};
+
+async function getJoke() {
+    const res = await request(options);
+    return res.joke;
+}
+
+module.exports = getJoke;
